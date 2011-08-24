9d802dfd49c6aaa9a7788494e9a4e31f69795b53
Tracey Eubanks <traceyeubanks@yahoo.com>
2011-08-24 15:35:24 -0600

    summing up most commonly categorized category

---
+++ script/commonly_categorized/query.rb
@@ -0,0 +1,10 @@
+def run_query(source)
+  reduce = File.read(Utilio::Path.root('script', 'commonly_categorized', 'reduce.js'))
+  query = {:key => 'category_guid',
+           :initial => {:count => 0},
+           :reduce => reduce
+  }
+  puts "query: #{query}"
+  results = source.group(query)
+  puts results.sort {|one,two| one['count'] <=> two['count']}.last
+end
